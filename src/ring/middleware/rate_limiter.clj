c49c846dd0e64cfa1a2fbeb2bf55ae43e7e8adfc
moo.hzq <zhengquan.hu@qq.com>
2018-02-14 15:26:26 +0800

    update

--- src/ring/middleware/rate_limiter.clj
+++ src/ring/middleware/rate_limiter.clj
@@ -12,14 +12,14 @@
     (if (and (number? interval) (number? max-in-interval))
       (let [now-key (-> (System/currentTimeMillis) (/ interval 1000) long)
             rl-key (get-in request key-in-request)]
-        (when debug
-          (println :rate-limiter :debug :rl-key rl-key :now-key now-key :count (get @user-counter rl-key)))
         (when (not= now-key @time-key)
           (reset! user-counter {})
           (reset! time-key now-key))
         (if (contains? @user-counter rl-key)
           (swap! user-counter update rl-key inc)
           (swap! user-counter assoc rl-key 1))
+        (when debug
+          (println :rate-limiter :debug :rl-key rl-key :now-key now-key :count (get @user-counter rl-key)))
         (if (> max-in-interval (get @user-counter rl-key 0))
           (handler request)
           {:status 429 :body fail-response}))
